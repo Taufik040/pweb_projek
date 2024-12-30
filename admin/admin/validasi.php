36201234e2162e3f9eda2cb5ee26404db799ef15
Alfian57 <alfiangadingsaputra@gmail.com>
2024-12-31 01:25:20 +0700

    fix: mysql

--- admin/admin/validasi.php
+++ admin/admin/validasi.php
@@ -1,10 +1,19 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "pweb_projek";
+if (file_exists('.env')) {
+    $env = parse_ini_file('.env');
+    $dbHost = $env["DB_HOST"];
+    $dbUsername = $env["DB_USERNAME"];
+    $dbPassword = $env["DB_PASSWORD"];
+    $dbName = $env["DB_NAME"];
+} else {
+    $dbHost = getenv("DB_HOST");
+    $dbUsername = getenv("DB_USERNAME");
+    $dbPassword = getenv("DB_PASSWORD");
+    $dbName = getenv("DB_NAME");
+}
 
-$koneksi = new mysqli($host, $user, $pass, $db_name);
+
+$koneksi = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
 if ($koneksi->connect_error) {
     die("Koneksi gagal: " . $koneksi->connect_error);
